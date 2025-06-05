2b1169ab27148154be11d734330c0c5169212928
j2207 <j2207@matsue-ct.ac.jp>
2025-06-05 08:08:31 +0000

    en_to9

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res) {
+router.get("/", function (req, res,next) {
+  try {
   res.render("index", { 
     title: "ローカルライブラリへようこそ" ,
     book_count: 120,
       book_instance_count: 240,
       book_instance_available_count: 45,
       author_count: 60,
-      genre_count: 15,});
-   　
+      genre_count: 15,
+    });
+  　} catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
