c4b04d96f317818435695201f502a764a184f35a
j2207 <j2207@matsue-ct.ac.jp>
2025-06-05 08:05:04 +0000

    en_to8

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.render("index", { title: "ローカルライブラリへようこそ" });
+  res.render("index", { 
+    title: "ローカルライブラリへようこそ" ,
+    book_count: 120,
+      book_instance_count: 240,
+      book_instance_available_count: 45,
+      author_count: 60,
+      genre_count: 15,});
+   　
 });
 
 module.exports = router;
